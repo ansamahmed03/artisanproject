c635daaa3a7bcdfd9970a55ded543b8664f3a256
ansamahmed03 <aahmed24@smail.ucas.edu.ps>
2026-04-12 15:53:44 +0300

    user table+artisan

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('userable_id');
+          $table->string('userable_type');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
